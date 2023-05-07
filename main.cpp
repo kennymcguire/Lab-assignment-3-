783ccfc2a5e561122ad9f82c1e1da5cb239d4f93
kennymcguire <kennymcguire72@gmail.com>
2023-05-07 18:13:53 +0000

    First Commit Lab Assignment 3

---
+++ main.cpp
@@ -0,0 +1,36 @@
+//Uses the measured z-acceleration to drive leds 2 and 3 of the mbed
+
+#include "mbed.h"
+#include "MMA7660.h"
+#include <cmath>
+
+#define pi 3.141592654
+
+MMA7660 MMA(p28, p27);
+
+DigitalOut connectionLed(LED1);
+
+
+float calculateAngle(float x, float y, float z){
+
+    float angle = 0;
+    float bott_part = sqrt((y*y) + (z*z));
+
+    angle = atan(x/bott_part);
+    angle = (angle*180.0)/pi;
+
+    return angle;   
+
+} 
+
+int main() {  
+    if (MMA.testConnection())
+        connectionLed = 1;
+        
+    while(1) {
+        printf("angle is %f \r\n", calculateAngle(MMA.x(), MMA.y(), MMA.z()));
+        wait(1);
+
+    }
+
+}
